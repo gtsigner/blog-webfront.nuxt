2e0f6ac56c3259145eb36d31c04d16958dc7df6b
zhaojun <zhaojunlike@gmail.com>
2018-08-29 23:19:36 +0800

    Fixbug:环境变量commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,12 @@ module.exports = {
             script: "./app.js",
             env: {
                 NODE_ENV: "development",
-                API_BASE: "http://127.0.0.1:7001/api/",
+                API_BASE: "http://127.0.0.1:7001/api/v1",
                 PORT: 3000
             },
             env_production: {
                 NODE_ENV: "production",
-                API_BASE: "https://blog.oeynet.com/api/",
+                API_BASE: "https://blog.oeynet.com/api/v1",
                 PORT: 3000
             }
         }
